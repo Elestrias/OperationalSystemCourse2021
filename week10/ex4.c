7dc20f2b7545e14765eb5a2727bb32b34a734089
elestrias <rus8-2002@mail.ru>
2021-10-26 23:59:54 +0300

    Update ex4

--- week10/ex4.c
+++ week10/ex4.c
@@ -1,4 +1,26 @@
-//
-// Created by Ruslan Gilvanov  on 26.10.2021.
-//
+#include <dirent.h>
+#include <stdio.h>
 
+int main() {
+    struct DIR *dirp = opendir("./tmp/");
+    struct dirent *dp = NULL;
+
+    char *fileNames[255];
+    unsigned long inodeNumbers[255];
+    int size = 0;
+
+    while ((dp = readdir(dirp)) != NULL) {
+        inodeNumbers[size] = dp->d_ino;
+        fileNames[size] = dp->d_name;
+        for(int i = 0; i < size; ++i)
+        {
+            if (inodeNumbers[size] == inodeNumbers[i]) {
+                printf("Hard links: %s - %s\n", fileNames[size], fileNames[i]);
+            }
+        }
+        ++size;
+    }
+
+    (void) closedir(dirp);
+    return 0;
+}
