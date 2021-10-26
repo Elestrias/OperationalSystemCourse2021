22030c8c5c78540cf2f4206094fe3a1b4dbeeba9
elestrias <rus8-2002@mail.ru>
2021-10-26 23:40:37 +0300

    Update ex2.sh

--- week10/ex2.sh
+++ week10/ex2.sh
@@ -1,4 +1,6 @@
-mkdir ../week01
+mkdir -p ../week01
 echo Ruslan Gilvanov> ../week01/file.txt
 link ../week01/file.txt ../week10/_ex2.txt
-find ../ -inum $ls -i ../week01/file.txt
+
+find ../ -inum $(ls -li ../week01/file.txt | awk '{print $1}') > ex2.txt
+find ../ -inum $(ls -li ../week01/file.txt | awk '{print $1}') -exec rm {} \; >> ex2.txt
